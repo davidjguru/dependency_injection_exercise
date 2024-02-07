653b996452bd3fa41f1423bedb1cb2a9d8a4e5c1
David Rodriguez <davidjguru@gmail.com>
2024-02-07 11:21:48 +0100

    refactor: add new service and its interface

---
+++ src/Service/DependencyInjectionExerciseServiceInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\dependency_injection_exercise\Service;
+
+/**
+ * Dependency Injection Exercise Service Interface.
+ *
+ * PHP version 8.1.23
+ *
+ * @category Class
+ * @package DependencyInjectionExercise
+ * @author David Rodríguez, @davidjguru
+ * @link https://therussianlullaby.com
+ */
+interface DependencyInjectionExerciseServiceInterface {
+
+  /**
+   * Get resources from external API connections.
+   *
+   * @param bool|null  $randomize
+   *   Boolean indicating if use fixed value for results or not.
+   * @return array
+   *   Returns a set of results.
+   */
+  public function getResources(bool $randomize): array;
+
+  /**
+   * Show resources from external API connections.
+   *
+   * @param bool|null  $randomize
+   *   Boolean indicating if use fixed value for results or not.
+   * @return array
+   *   Returns a set of images.
+   */
+  public function showPhotos(bool $randomize): array;
+
+}
