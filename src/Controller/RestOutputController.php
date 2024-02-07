f752ed9a4db0af2ac5283a77e19199e6d2e8e780
David Rodriguez <davidjguru@gmail.com>
2024-02-07 14:33:08 +0100

    refactor: modify the original controller

--- src/Controller/RestOutputController.php
+++ src/Controller/RestOutputController.php
@@ -2,58 +2,64 @@
 
 namespace Drupal\dependency_injection_exercise\Controller;
 
-use Drupal\Component\Serialization\Json;
 use Drupal\Core\Controller\ControllerBase;
-use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\dependency_injection_exercise\Service\DependencyInjectionExerciseService;
+use Drupal\dependency_injection_exercise\Service\DependencyInjectionExerciseServiceInterface;
 
 /**
- * Provides the rest output.
+ * RestOutputController Class.
+ *
+ * Manage the REST output to screen.
+ *
+ * PHP version 8.1.23
+ *
+ * @category Class
+ * @package DependencyInjectionExerciseyo
+ * @author Dropsolid + David Rodríguez, @davidjguru
+ * @link https://therussianlullaby.com
  */
 class RestOutputController extends ControllerBase {
 
   /**
+   * The DependencyInjectionExerciseService resource.
+   *
+   * @var \Drupal\dependency_injection_exercise\Service\DependencyInjectionExerciseService
+   */
+  protected $diesResource;
+
+  /**
+   * RestOutputController constructor.
+   *
+   * @param \Drupal\dependency_injection_exercise\Service\DependencyInjectionExerciseServiceInterface $dies_resource
+   *   The dependency injection exercise service.
+   */
+  public function __construct(DependencyInjectionExerciseServiceInterface $dies_resource) {
+    $this->diesResource = $dies_resource;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The Drupal service container.
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('dependency_injection_exercise.api_client')
+    );
+  }
+  /**
    * Displays the photos.
    *
    * @return array[]
-   *   A renderable array representing the photos.
+   *   A renderable array representing the photos or error messages.
    */
-  public function showPhotos(): array {
-    // Setup build caching.
-    $build = [
-      '#cache' => [
-        'max-age' => 60,
-        'contexts' => [
-          'url',
-        ],
-      ],
-    ];
-
-    // Try to obtain the photo data via the external API.
-    try {
-      $response = \Drupal::httpClient()->request('GET', 'https://jsonplaceholder.typicode.com/albums/5/photos');
-      $raw_data = $response->getBody()->getContents();
-      $data = Json::decode($raw_data);
-    }
-    catch (GuzzleException $e) {
-      $build['error'] = [
-        '#type' => 'html_tag',
-        '#tag' => 'p',
-        '#value' => $this->t('No photos available.'),
-      ];
-      return $build;
-    }
-
-    // Build a listing of photos from the photo data.
-    $build['photos'] = array_map(static function ($item) {
-      return [
-        '#theme' => 'image',
-        '#uri' => $item['thumbnailUrl'],
-        '#alt' => $item['title'],
-        '#title' => $item['title'],
-      ];
-    }, $data);
-
-    return $build;
+  public function getPhotos(): array {
+    // Call to the processing method from Service.
+    return $diesResource->showPhotos();
   }
 
 }
